589763bd6674b625062c6f2ebf7ef94e57bf6b0f
snowyaya <yayingliang@gmail.com>
2022-03-07 21:43:05 -0500

    updated textures

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -3,6 +3,75 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
+ * Texture
+ */
+// const image = new Image()
+// const texture = new THREE.Texture(image)
+const loadingManager = new THREE.LoadingManager()
+
+// loadingManager.onStart = () => {
+//     console.log("on loaded...")
+// }
+// loadingManager.onProgress = () => {
+//     console.log("on progress...")
+// }
+// loadingManager.onError = () => {
+//     console.log("on error...")
+// }
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTexture = textureLoader.load('textures/minecraft.png')
+// const alphaTexture = textureLoader.load('textures/door/alpha.jpg')
+// const heightTexture = textureLoader.load('textures/door/height.jpg')
+// const normalTexture = textureLoader.load('textures/door/normal.jpg')
+// const ambientOcclusionTexture = textureLoader.load('textures/door/ambientOcclusion.jpg')
+// const metalnessTexture = textureLoader.load('textures/door/metalness.jpg')
+// const roughnessTexture = textureLoader.load('textures/door/roughness.jpg')
+
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.RepeatWrapping
+// colorTexture.wrapT = THREE.RepeatWrapping
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+
+// colorTexture.rotation = Math.PI / 4
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+colorTexture.generateMipmaps = false
+colorTexture.minFilter = THREE.NearestFilter
+colorTexture.magFilter = THREE.NearestFilter
+
+/**
+const texture = textureLoader.load(
+    '/textures/door/color.jpg',
+    () => {
+        // load
+    },
+    () => {
+        // progress
+    },
+    () => {
+        // error
+    }
+)
+ */
+
+/**
+image.onload = () => {
+    // // create the texture (can only be used inside this function)
+    // const texture = new THREE.Texture(image)
+
+    texture.needsUpdate = true // update the texture
+
+}
+image.src = '/textures/door/color.jpg'
+ */
+
+
+/**
  * Base
  */
 // Canvas
@@ -15,7 +84,9 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// use the texture that is defined earlier to the current object
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -27,8 +98,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -70,8 +140,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
