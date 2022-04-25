1b571e0e86f8555b2a1eed7269b25d624b5e32ba
snowyaya <yayingliang@gmail.com>
2022-04-25 13:49:03 -0400

    Add shadow

--- 16-shadows/src/script.js
+++ 16-shadows/src/script.js
@@ -21,7 +21,7 @@ const scene = new THREE.Scene()
 // Ambient light
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
-scene.add(ambientLight)
+scene.add(ambientLight) // remove this would make the object very dark
 
 // Directional light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
@@ -31,6 +31,35 @@ gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
+
+directionalLight.castShadow = true // let the object cast shadow
+directionalLight.shadow.mapSize.width = 1024
+directionalLight.shadow.mapSize.height = 1024
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.bottom = -2
+directionalLight.shadow.camera.left = -2
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 6
+
+directionalLight.shadow.radius = 10 // control the shadow blur with the radius property
+
+console.log(directionalLight.shadow.camera) // get the size
+
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(directionalLightCameraHelper)
+
+// Add Spot light
+const spotLight = new THREE.SpotLight(0xffffff, 0.4, 10, Math.PI * 0.3)
+
+spotLight.castShadow = true
+
+spotLight.position.set(0, 2, 2)
+scene.add(spotLight)
+scene.add(spotLight.target)
+
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+scene.add(spotLightCameraHelper)
 
 /**
  * Materials
@@ -55,6 +84,9 @@ const plane = new THREE.Mesh(
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
 
+sphere.castShadow = true
+plane.receiveShadow = true;
+
 scene.add(sphere, plane)
 
 /**
@@ -65,8 +97,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -75,7 +106,7 @@ window.addEventListener('resize', () =>
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-    // Update renderer
+    // Update renderer 
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
@@ -103,13 +134,15 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+renderer.shadowMap.enabled = true // !!! enable the shadow!!!
+renderer.shadowMap.type = THREE.PCFSoftShadowMap // this would lose the radius
+
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
